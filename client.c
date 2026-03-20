1d1fe98bde99ac68b107f6bb0901e61c2a9387b5
Léon Dubau <leondubau@MacBook-Air-de-Leon.local>
2026-03-20 07:50:17 +0100

    minitalk

--- client.c
+++ client.c
@@ -2,31 +2,54 @@
 
 #include <stdio.h>
 
+volatile sig_atomic_t g_ack = 0;
+
+void	ack_handler(int signal)
+{
+	(void)signal;
+	g_ack = 1;
+}
+
 int main(int ac, char **av)
 {
+	struct sigaction	action;
 	int	pid;
 	char	*signal;
 	int	i;
 	int	j;
 
-	if (ac < 2)
+	if (ac != 3)
 		return (0);
+	action.sa_handler = ack_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
 	pid = atoi(av[1]);
 	signal = av[2];
 	i = 0;
+	sigaction(SIGUSR1, &action, NULL);
 	while (signal[i])
 	{
 		j = 0;
+		g_ack = 0;
 		while (j < 8)
 		{
-			if ((signal[i] >> j & 1) == 0)
-				kill(pid, SIGUSR1);
-			else if ((signal[i] >> j & 1) == 1)
+			if (signal[i] >> j & 1)
 				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
 			j++;
-			usleep(100);
 		}
+		while (g_ack == 0)
+				pause();
+			usleep(100);
 		i++;
+	}
+	while (j > 0)
+	{
+		kill(pid, SIGUSR1); // 
+		pause();
+		usleep(100);
+		j--;
 	}
 	return 0;
 }
--- serveur.c
+++ serveur.c
@@ -2,29 +2,38 @@
 
 #include <stdio.h>
 
-void	handler(int	signal)
+char *global;
+
+void	handler(int signal, siginfo_t *info, void *context)
 {
 	static int	pos;
 	static char	c;
 
+	(void) context;
 	if (signal == SIGUSR2)
 		c |= 1 << pos;
 	pos++;
 	if (pos == 8)
 	{
+		if (!c)
+			write(1, "\n", 1);
 		write(1, &c, 1);
 		pos = 0;
 		c = 0;
+		kill(info->si_pid, SIGUSR1);
 	}
 }
 
-int main()
+int main(int ac, char **av)
 {
 	struct sigaction	action;
 
-	action.sa_handler = handler;
+	if (ac != 1)
+		printf("Error\n");
+	(void)av;
+	action.sa_sigaction = handler;
 	sigemptyset(&action.sa_mask);
-	action.sa_flags = 0;
+	action.sa_flags = SA_SIGINFO;
 	printf("PID : %d\n", getpid());
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
