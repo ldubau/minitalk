f7651b58dc759bcfaf99fa9d0426daeb35bf722f
Leon Dubau <ldubau@2D7.42angouleme.fr>
2026-03-26 15:22:12 +0100

    minitalk

---
+++ client_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leondubau <leondubau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/23 17:39:52 by leondubau         #+#    #+#             */
+/*   Updated: 2026/03/24 20:53:49 by leondubau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+volatile int	g_ack = 0;
+
+int	error(void)
+{
+	write(1, "Error\n", 6);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		nbr;
+	int		sign;
+	size_t	i;
+
+	nbr = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || ('\t' <= str[i] && str[i] <= '\r'))
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr * sign);
+}
+
+void	ack_handler(int signal)
+{
+	(void)signal;
+	g_ack = 1;
+}
+
+void	minitalk(int i, int j, int pid, char *signal)
+{
+	int	last_char;
+
+	last_char = 1;
+	while (last_char)
+	{
+		if (signal[i] == 0)
+			last_char = 0;
+		j = 0;
+		g_ack = 0;
+		while (j < 8)
+		{
+			if (signal[i] >> j & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(200);
+			while (g_ack == 0)
+				pause();
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	struct sigaction	action;
+	char				*signal;
+	int					pid;
+	int					i;
+	int					j;
+
+	if (ac != 3)
+		return (error());
+	action.sa_handler = ack_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	pid = ft_atoi(av[1]);
+	signal = av[2];
+	i = 0;
+	j = 0;
+	sigaction(SIGUSR1, &action, NULL);
+	minitalk(i, j, pid, signal);
+	return (0);
+}
---
+++ server_bonus.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldubau <ldubau@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/23 17:39:45 by leondubau         #+#    #+#             */
+/*   Updated: 2026/03/26 12:19:47 by ldubau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+char	*g_string = NULL;
+
+char	*ft_realloc(char *g_string, char c)
+{
+	char	*new_string;
+	int		i;
+
+	i = 0;
+	if (!g_string)
+	{
+		g_string = malloc(sizeof(char));
+		g_string[0] = '\0';
+	}
+	while (g_string[i])
+		i++;
+	new_string = malloc(sizeof(char) * ft_strlen(g_string) + 2);
+	ft_strlcpy(new_string, g_string, ft_strlen(g_string) + 1);
+	free(g_string);
+	new_string[i] = c;
+	new_string[i + 1] = '\0';
+	return (new_string);
+}
+
+int	end_string()
+{
+	ft_printf("%s\n", g_string);
+	free(g_string);
+	g_string = NULL;
+	return (0);
+}
+
+void	handler(int signal, siginfo_t *info, void *context)
+{
+	static int	pos;
+	static char	c;
+	static int	current_pid;
+
+	(void) context;
+	if (current_pid == 0)
+		current_pid = info->si_pid;
+	if (info->si_pid != current_pid)
+	{
+		pos = 0;
+		c = 0;
+		current_pid = info->si_pid;
+	}
+	if (signal == SIGUSR2)
+		c |= 1 << pos;
+	pos++;
+	if (pos == 8 && c)
+	{
+		g_string = ft_realloc(g_string, c);
+		pos = 0;
+		c = 0;
+	}
+	else if (pos == 8 && !c)
+		end_string();
+	kill(info->si_pid, SIGUSR1);
+}
+
+int	main(int ac, char **av)
+{
+	struct sigaction	action;
+
+	if (ac != 1)
+	{
+		write(2, "Error\n", 6);
+		return (1);
+	}
+	(void)av;
+	action.sa_sigaction = handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = SA_SIGINFO;
+	ft_printf("PID : %d\n", getpid());
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+	while (1)
+		pause();
+	return (0);
+}
+
+
+// if (pos == 8)
+// 	{
+// 		if (!c)
+// 			end_string();
+// 		else
+// 			g_string = ft_realloc(g_string, c);
+// 		pos = 0;
+// 		c = 0;
+// 	}
